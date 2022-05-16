b6ca910ec705407404b7743873af6d7702bd56b4
Priya Shukla <41220395+priyology@users.noreply.github.com>
2022-05-16 08:44:43 -0700

    updated power level based on convo with ted

--- scripts/PwrAnalysis_Carbonyl.R
+++ scripts/PwrAnalysis_Carbonyl.R
@@ -2,7 +2,18 @@
 
 library(pwr)
 # calculate minimal sample size
-pwr.anova.test(k=5,            # 5 groups are compared
-               f=.25,          # moderate effect size
-               sig.level=.05,  # alpha/sig. level = .05
-               power=.8)       # confint./power = .8
+pwr.anova.test(k = 16,            # 4 groups (MHW treatment) are compared
+               f = .25,          # moderate effect size
+               sig.level = .05,  # alpha/sig. level = .05
+               n = 15)          # n of participants
+
+## power = 0.64
+
+
+
+pwr.anova.test(k = 16,            # 4 groups (MHW treatment) are compared
+               f = .25,          # moderate effect size
+               sig.level = .05,  # alpha/sig. level = .05
+               power = 0.8)     # desired power level 
+
+## n = 19.6 (~ 20)
