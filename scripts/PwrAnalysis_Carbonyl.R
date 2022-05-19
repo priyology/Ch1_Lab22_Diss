9d430219fe96ae9a02fb72d445facb2bb3d6c947
Priya Shukla <41220395+priyology@users.noreply.github.com>
2022-05-19 09:09:32 -0700

    fixed power analysis

--- scripts/PwrAnalysis_Carbonyl.R
+++ scripts/PwrAnalysis_Carbonyl.R
@@ -2,18 +2,18 @@
 
 library(pwr)
 # calculate minimal sample size
-pwr.anova.test(k = 16,            # 16 groups (MHW x SH_Temp x SH_Tide) are compared
+pwr.anova.test(k = 4,            # 4 groups (MHW x SH_Temp x SH_Tide) are compared
                f = .25,          # moderate effect size
                sig.level = .05,  # alpha/sig. level = .05
-               n = 15)          # n of participants
+               n = 50)          # n of oysters (5 per tank, 10 tanks per heatwave)
 
-## power = 0.64
+## power = 0.84
 
 
 
-pwr.anova.test(k = 16,            # 16 groups (MHW x SH_Temp x SH_Tide) are compared
+pwr.anova.test(k = 4,            # 16 groups (MHW x SH_Temp x SH_Tide) are compared
                f = .25,          # moderate effect size
                sig.level = .05,  # alpha/sig. level = .05
                power = 0.8)     # desired power level 
 
-## n = 19.6 (~ 20)
+## n = 45
