34263b02839f703dc388f0286aa494ca703ffb9b
Priya Shukla <41220395+priyology@users.noreply.github.com>
2023-01-31 20:32:40 -0800

    added data files, code to graph O_lurida outplanting data, and a figure + powerpoint export code

--- scripts/Mortality_Basic.R
+++ scripts/Mortality_Basic.R
@@ -3,7 +3,7 @@
 library(tidyverse)
 library(ggdark)
 
-### Stress Hardening
+#### Stress Hardening ==============
 
 SH_mort <- read_csv("data/SH_mortality.csv")
 str(SH_mort)
@@ -38,7 +38,7 @@ labs(
     ) +
 theme_classic()
 
-### MHW
+#### MHW =============
 
 MHW_mort <- read_csv("data/MHW_Mortality.csv")
 str(MHW_mort)
@@ -56,6 +56,7 @@ MHW_mort_summ <- MHW_mort %>%
     total = n_distinct(Mortality, na.rm = TRUE),
     mean = mean(Mortality, na.rm = TRUE))
 MHW_mort_summ
+print(MHW_mort_summ, n = 48)
 
 ## Graphs
 ggplot(MHW_mort_summ, aes(x = SH_tide, y = total, fill = Species, group = SH_Temp)) +
@@ -67,3 +68,65 @@ ggplot(MHW_mort_summ, aes(x = SH_tide, y = total, fill = Species, group = SH_Tem
     title = "Oyster Mortality After Marine Heatwaves"
   ) +
   theme_classic()
+
+#### O. lurida outplanting =========
+Outplant_mort <- read_csv("data/Olurida_OutplantingMortality.csv")
+str(Outplant_mort)
+Outplant_mort$SH_Temp <- as.character(Outplant_mort$SH_Temp) #make a character
+head(Outplant_mort)
+tail(Outplant_mort)
+
+is.na(Outplant_mort) #are there NAs = no
+sum(is.na(Outplant_mort)) # how many NAs = 0
+sapply(Outplant_mort, function(x) sum(is.na(x))) #NAs by columns
+
+## Graphs
+
+outplantsurvival.plot <- ggplot(Outplant_mort, aes(x = SH_Temp, y = Survival_Numb, fill = SH_Temp)) +
+  geom_bar(stat = "identity") +
+  facet_wrap(~SH_Tide) +
+  scale_fill_brewer(palette = "Set2") +
+  labs(
+    x = "Temperature (\u00B0C)",
+    y = "Number Survived",
+    title = "Olympia Oyster Survival 279 Days After Outplanting"
+  ) +
+  dark_theme_classic()
+
+outplantsurvival.plot
+
+#### PowerPoint Editing Prep: Outplanting Plot ==============
+
+#### officeR directly exports the plot to your desired file into a powerpoint slide-shaped image ===== 
+library(officer)
+
+## initialize R object representing .pptx file. 
+OutplantingSurvival_fig <- read_pptx()
+OutplantingSurvival_fig <- add_slide(OutplantingSurvival_fig, layout = "Title and Content", master = "Office Theme")
+OutplantingSurvival_fig <- ph_with(x = OutplantingSurvival_fig, value = outplantsurvival.plot, location = ph_location_fullsize() )
+OutplantingSurvival_fig <- ph_with(x = OutplantingSurvival_fig, "O. lurida Survival", location = ph_location_type(type = "title") )
+print(OutplantingSurvival_fig, target = "presentations/OluridaOutplantingSurvival.pptx")
+
+#### R2PPT / RDCOMClient =====
+
+#install.packages("devtools", dependencies = TRUE)
+
+library(RDCOMClient)
+library(R2PPT)
+
+## Step 1: Save as a temporary file
+TEMP_FILE <- paste(tempfile(), ".wmf", sep="")
+ggsave(TEMP_FILE, plot = outplantsurvival.plot) # Saving the plot to the temporary file
+
+## Step 2: Open a blank PPT slide
+mkppt <- PPT.Init (method = "RDCOMClient")
+mkppt <- PPT.AddBlankSlide(mkppt)
+
+## Step 3: Export graph to PPT slide
+mkppt <- PPT.AddGraphicstoSlide(mkppt, file = TEMP_FILE)
+
+unlink(TEMP_FILE)
+
+## once in PPT, right-click on image and select "edit picture" to futz with individual components of the graph
+
+
