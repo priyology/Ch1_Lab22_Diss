adb326427060459c354fd7b3dfe408aacd59bf20
Priya Shukla <prishukla.sci@gmail.com>
2023-02-06 19:45:20 -0800

    modified code to deal with issues with plotting & running stats

--- scripts/CI_Csikamea.R
+++ scripts/CI_Csikamea.R
@@ -2,11 +2,217 @@
 
 ## load libraries
 library(tidyverse)
-library(sjPlot)
-library(lme4)
+library(sjPlot) ## manuscript-quality tables
+library(lme4) ##glm
+library(lmerTest) ##p-values
 
 ### load data sheet
 Csikamea_CI_og <- read_csv("data/C_sikamea/CI_Csikamea.csv")
 glimpse(Csikamea_CI_og)
-summary(Olurida_CI_og)
-View(Olurida_CI_og)
+summary(Csikamea_CI_og)
+View(Csikamea_CI_og)
+
+### new variable for new factors
+Csikamea_CI1 <- Csikamea_CI_og
+
+### change attributes about statistical factors
+Csikamea_CI1$CI <- as.numeric(Csikamea_CI1$CI) ## make CI numeric
+is.numeric(Csikamea_CI1$CI) ## True
+Csikamea_CI1$SH_Temp <- as.character(Csikamea_CI1$SH_Temp) ## character
+is.character(Csikamea_CI1$SH_Temp)
+Csikamea_CI1$SH_Tide <- as.character(Csikamea_CI1$SH_Tide) ## character
+is.character(Csikamea_CI1$SH_Tide)
+Csikamea_CI1$MHW <- as.character(Csikamea_CI1$MHW) ## character
+is.character(Csikamea_CI1$MHW)
+
+#### Check for DEAD oysters =========
+DEAD_Csikamea_CI <- filter(Csikamea_CI1, DEAD == "DEAD")
+nrow(DEAD_Csikamea_CI)
+glimpse(DEAD_Csikamea_CI)
+summary(DEAD_Csikamea_CI)
+
+### CSV of dead oysters
+write_csv(DEAD_Csikamea_CI , file = "data/C_sikamea/CI_Csikamea_Dead.csv") #Dead Oysters
+
+DEAD_count <- DEAD_Csikamea_CI %>% group_by(SH_Temp, SH_Tide, MHW) %>% 
+  summarize("Numb_Dead" = n())
+DEAD_count
+
+## CSV of counts dead oysters (NOTE: going into data_output)
+write_csv(DEAD_count, file = "data_output/C_sikamea/DEAD_Csikamea_CI.csv")
+
+
+#### Omit NAs=========
+
+#### now remove NAs from data sheet
+colSums(is.na(Csikamea_CI1)) ## find NAs in each column, 93 under CI
+Csikamea_CI2 <- Csikamea_CI1 %>% 
+  filter(!is.na(CI)) # omit the 93 NAs in CI
+
+colSums(is.na(Csikamea_CI2)) ## 0 NAs under CI
+
+glimpse(Csikamea_CI2)
+summary(Csikamea_CI2)
+
+## CSV without NAs (empty cells / dead oysters)
+write_csv(Csikamea_CI2 , file = "data/C_sikamea/Csikamea_CI_noNAs.csv")
+
+#histogram of all CIs
+ggplot(Csikamea_CI2, aes(CI)) +
+  geom_histogram() +
+  theme_classic() +
+  scale_fill_brewer(palette = "RdYlBu", direction = -1) +
+  labs(title = "CI Histogram",
+       x = "CI",
+       y = "Counts of CI")
+
+Csikamea_HighCI <- filter(Csikamea_CI2, CI > 10)
+nrow(Csikamea_HighCI)
+
+Csikamea_LowCI <- filter(Csikamea_CI2, CI < 0)
+nrow(Csikamea_LowCI)
+
+#Csikamea_CI2
+
+#### Figures, Mean, SD, SE =====
+
+#### No Grouping ====
+Stats_ALL <- Csikamea_CI2 %>%
+  summarize(
+    Mean_Shell.g = mean(Shell.g),
+    SD_Shell.g = sd(Shell.g),
+    SE_Shell.g = SD_Shell.g/sqrt(n()),
+    Mean_Tissue_g = mean(Tissue.g),
+    SD_Tissue.g = sd(Tissue.g),
+    SE_Tissue.g = SD_Tissue.g/sqrt(n()),
+    Mean_CI = mean(CI),
+    SD_CI = sd(CI),
+    SE_CI = SD_CI/sqrt(n()))
+
+Stats_ALL
+
+#### Grouped by MHW ====
+Stats_MHW <- Csikamea_CI2 %>%
+  group_by(MHW) %>% 
+  summarize(
+    Mean_Shell.g = mean(Shell.g),
+    SD_Shell.g = sd(Shell.g),
+    SE_Shell.g = SD_Shell.g/sqrt(n()),
+    Mean_Tissue_mg = mean(Tissue.g),
+    SD_Tissue.g = sd(Tissue.g),
+    SE_Tissue.g = SD_Tissue.g/sqrt(n()),
+    Mean_CI = mean(CI),
+    SD_CI = sd(CI),
+    SE_CI = SD_CI/sqrt(n()))
+
+Stats_MHW
+
+## plot
+ggplot(Csikamea_CI2, aes(x=MHW, y=CI, fill = MHW)) + #factor MHW
+  geom_boxplot() +
+  theme_classic() +
+  scale_fill_brewer(palette = "RdYlBu", direction = -1)
+
+#### Grouped by SH_Tide ====
+Stats_SH_Tide <- Csikamea_CI2 %>%
+  group_by(SH_Tide) %>% 
+  summarize(
+    Mean_Shell.g = mean(Shell.g),
+    SD_Shell.g = sd(Shell.g),
+    SE_Shell.g = SD_Shell.g/sqrt(n()),
+    Mean_Tissue_mg = mean(Tissue.g),
+    SD_Tissue.g = sd(Tissue.g),
+    SE_Tissue.g = SD_Tissue.g/sqrt(n()),
+    Mean_CI = mean(CI),
+    SD_CI = sd(CI),
+    SE_CI = SD_CI/sqrt(n()))
+
+Stats_SH_Tide
+
+## plot
+ggplot(Csikamea_CI2, aes(x=SH_Tide, y = CI, fill = SH_Tide)) +
+  geom_boxplot() +
+  theme_classic() +
+  scale_fill_brewer(palette = "Paired", direction = -1)
+
+#### Grouped by SH_Temp ====
+
+Stats_SHTemp <- Csikamea_CI2 %>%
+  group_by(SH_Temp) %>% 
+  summarize(
+    Mean_Shell.g = mean(Shell.g),
+    SD_Shell.g = sd(Shell.g),
+    SE_Shell.g = SD_Shell.g/sqrt(n()),
+    Mean_Tissue_mg = mean(Tissue.g),
+    SD_Tissue.g = sd(Tissue.g),
+    SE_Tissue.g = SD_Tissue.g/sqrt(n()),
+    Mean_CI = mean(CI),
+    SD_CI = sd(CI),
+    SE_CI = SD_CI/sqrt(n()))
+
+Stats_SHTemp
+
+## plot
+ggplot(Csikamea_CI2, aes(x=SH_Temp, y=CI, fill = SH_Temp)) +
+  geom_boxplot() +
+  theme_classic() +
+  scale_fill_manual(values=c("#4575B4", "#FDAE61"))
+
+#### Grouped by SH_Tide, SH_Temp ====
+Stats_SH_TideTemp <- Csikamea_CI2 %>%
+  group_by(SH_Tide, SH_Temp) %>% 
+  summarize(
+    Mean_Shell.g = mean(Shell.g),
+    SD_Shell.g = sd(Shell.g),
+    SE_Shell.g = SD_Shell.g/sqrt(n()),
+    Mean_Tissue_mg = mean(Tissue.g),
+    SD_Tissue.g = sd(Tissue.g),
+    SE_Tissue.g = SD_Tissue.g/sqrt(n()),
+    Mean_CI = mean(CI),
+    SD_CI = sd(CI),
+    SE_CI = SD_CI/sqrt(n()))
+
+Stats_SH_TideTemp
+
+## plot
+ggplot(Csikamea_CI2, aes(x=SH_Temp, y=CI, fill = SH_Temp)) +
+  geom_boxplot() +
+  facet_wrap(~SH_Tide) +
+  theme_classic() +
+  scale_fill_manual(values=c("#4575B4", "#FDAE61"))
+#### Grouped by SH_Tide, SH_Temp, MHW ====
+Stats_SH_TideTemp_MHW <- Csikamea_CI2 %>%
+  group_by(SH_Tide, SH_Temp, MHW) %>% 
+  summarize(
+    Mean_Shell.g = mean(Shell.g),
+    SD_Shell.g = sd(Shell.g),
+    SE_Shell.g = SD_Shell.g/sqrt(n()),
+    Mean_Tissue_mg = mean(Tissue.g),
+    SD_Tissue.g = sd(Tissue.g),
+    SE_Tissue.g = SD_Tissue.g/sqrt(n()),
+    Mean_CI = mean(CI),
+    SD_CI = sd(CI),
+    SE_CI = SD_CI/sqrt(n()))
+
+Stats_SH_TideTemp_MHW
+
+View(Stats_SH_TideTemp_MHW)
+
+ggplot(Csikamea_CI2, aes(x=MHW, y=CI, fill = MHW)) +
+  geom_boxplot() +
+  facet_grid(SH_Tide ~ SH_Temp) +
+  theme_classic() +
+  theme(legend.position = "none") +
+  scale_fill_brewer(palette = "RdYlBu", direction = -1) +
+  labs(title =expression(paste("Condition Indices of ", italic("C.sikamea"))), 
+       x = "Marine Heatwave (°C)", 
+       y = "Condition Index") #, fill = " Marine Heatwave")
+
+ggsave(filename = "fig_output/Csikamea_CI.png",width = 5.10, height = 5.77, dpi = 300)
+
+## CSV for Mean, SD, SE: Stats_SH_TideTemp_MHW (folder: data_output)
+write_csv(Stats_SH_TideTemp_MHW , file = "data_output/C_sikamea/Csikamea_CI_MeanSDSE.csv")
+
+#### c. SIKAMEA STATS ===============
+
+
--- scripts/CI_Olurida.R
+++ scripts/CI_Olurida.R
@@ -2,8 +2,9 @@
 
 ## load libraries
 library(tidyverse)
-library(sjPlot)
-library(lme4)
+library(sjPlot) ## manuscript-quality tables
+library(lme4) ##glm
+library(lmerTest) ##p-values
 
 ### load data sheet
 Olurida_CI_og <- read_csv("data/O_lurida/CI_Olurida.csv")
@@ -11,33 +12,42 @@ glimpse(Olurida_CI_og)
 summary(Olurida_CI_og)
 View(Olurida_CI_og)
 
-### change attributes about statistical factors
-Olurida_CI1$CI <- as.numeric(Olurida_CI1$CI) ## make CI numeric
-is.numeric(Olurida_CI1$CI) ## True
-Olurida_CI3$fSH_Temp <- as.factor(Olurida_CI3$SH_Temp)
-is.factor(Olurida_CI3$fSH_Temp)
-Olurida_CI3$fSH_Tide <- as.factor(Olurida_CI3$SH_Tide)
-is.factor(Olurida_CI3$fSH_Tide)
-Olurida_CI3$fMHW <- as.factor(Olurida_CI3$MHW)
-is.factor(Olurida_CI3$fMHW)
+## setting these as.character/as.numeric
 
-## setting these as.character doesn't matter, since I've created factors above
-#Olurida_CI1$MHW <- as.character(Olurida_CI1$MHW) ## make MHW is a character
-#is.character(Olurida_CI1$MHW)
-#Olurida_CI1$SH_Temp <- as.character(Olurida_CI1$SH_Temp) ## make SH_Temp a character
-#is.character(Olurida_CI1$SH_Temp)
+Olurida_CI_og$CI <- as.numeric(Olurida_CI_og$CI) ## make CI numeric
+is.numeric(Olurida_CI_og$CI) ## True
+
+Olurida_CI_og$SH_Temp <- as.character(Olurida_CI_og$SH_Temp) ## make SH_Temp a character
+is.character(Olurida_CI_og$SH_Temp) ## True
+
+Olurida_CI_og$MHW <- as.character(Olurida_CI_og$MHW) ## make MHW is a character
+is.character(Olurida_CI_og$MHW) ## True
+
+
+### change attributes about statistical factors
+
+#Olurida_CI_og$fSH_Temp <- as.factor(Olurida_CI_og$SH_Temp)
+#is.factor(Olurida_CI_og$SH_Temp) ## True
+
+#Olurida_CI_og$fSH_Tide <- as.factor(Olurida_CI_og$SH_Tide)
+#is.factor(Olurida_CI_og$SH_Tide) ## True
+
+#Olurida_CI_og$fMHW <- as.factor(Olurida_CI_og1$MHW)
+#is.factor(Olurida_CI_og$MHW) ## True
+
+
+
 
 
 #### remove Tank 1 b/c foil boats incinerated in muffle furnace =======
 Olurida_CI1 <- filter(Olurida_CI_og, Tank > 1)
 summary(Olurida_CI1)
 
-
 ### CSV of data with Tank 1 excluded
 write_csv(Olurida_CI1, file = "data/O_lurida/CI_Olurida_noTank1.csv")
 
 #### Check for DEAD oysters =========
-DEAD_Olurida_CI <- filter(Olurida_CI_og, Notes == "*DEAD*")
+DEAD_Olurida_CI <- filter(Olurida_CI1, Notes == "*DEAD*")
 nrow(DEAD_Olurida_CI)
 glimpse(DEAD_Olurida_CI)
 summary(DEAD_Olurida_CI)
@@ -56,19 +66,20 @@ write_csv(DEAD_count, file = "data_output/O_lurida/DEAD_Olurida_CI.csv")
 
 #### now remove NAs from data sheet
 colSums(is.na(Olurida_CI1)) ## find NAs in each column, 4 under CI, 754 under Notes
-Olurida_CI2 <- Olurida_CI1 %>% select(-Notes) ## omit Notes column which is all NAs / info
-colSums(is.na(Olurida_CI2)) ## 6 NAs under CI
-Olurida_CI3 <- na.omit(Olurida_CI2) # omit the 6 NAs in CI
-colSums(is.na(Olurida_CI3)) ## all NAs omitted
 
-glimpse(Olurida_CI3)
-summary(Olurida_CI3)
+## omit Notes column which is all NAs / info
+Olurida_CI2 <- Olurida_CI1 %>% 
+  filter(!is.na(CI)) # omit the 93 NAs in CI
+colSums(is.na(Olurida_CI2)) ## All NAs under CI
+
+glimpse(Olurida_CI2)
+summary(Olurida_CI2)
 
 ## CSV without NAs (empty cells / dead oysters)
-write_csv(Olurida_CI3 , file = "data/O_lurida/Olurida_CI_noTank1_noNAs.csv")
+write_csv(Olurida_CI2 , file = "data/O_lurida/Olurida_CI_noTank1_noNAs.csv")
 
 #histogram of all CIs
-ggplot(Olurida_CI3, aes(CI)) +
+ggplot(Olurida_CI2, aes(CI)) +
   geom_histogram() +
   theme_classic() +
   scale_fill_brewer(palette = "RdYlBu", direction = -1) +
@@ -76,27 +87,26 @@ ggplot(Olurida_CI3, aes(CI)) +
        x = "CI",
        y = "Counts of CI")
 
-
-Olurida_HighCI <- filter(Olurida_CI3, CI > 25)
-nrow(Olurida_HighCI)
+Olurida_HighCI <- filter(Olurida_CI2, CI > 20)
+nrow(Olurida_HighCI) #19
 
 #### Outlier RESOLVED Feb 6, 2023 ========
 ## filter out CI > 50
-#Olurida_CI.outliers <- filter(Olurida_CI3, CI < 0 | CI > 50)
+#Olurida_CI.outliers <- filter(Olurida_CI2, CI < 0 | CI > 50)
 #Olurida_CI.outliers
 #View(Olurida_CI.outliers)
 
-#Olurida_CI3 <- filter(Olurida_CI3, between(CI, 0, 50))
-#View(Olurida_CI3)
+#Olurida_CI2 <- filter(Olurida_CI2, between(CI, 0, 50))
+#View(Olurida_CI2)
 
-#ggplot(Olurida_CI3, aes(x = MHW, y = CI)) + 
+#ggplot(Olurida_CI2, aes(x = MHW, y = CI)) + 
 #  geom_point() +
 #  theme_classic()
 
 #### Figures, Mean, SD, SE =====
 
 #### No Grouping ====
-Stats_ALL <- Olurida_CI3 %>%
+Stats_ALL <- Olurida_CI2 %>%
     summarize(
     Mean_Shell.mg = mean(Shell.mg),
     SD_Shell.mg = sd(Shell.mg),
@@ -111,7 +121,7 @@ Stats_ALL <- Olurida_CI3 %>%
 Stats_ALL
 
 #### Grouped by MHW ====
-Stats_MHW <- Olurida_CI3 %>%
+Stats_MHW <- Olurida_CI2 %>%
   group_by(MHW) %>% 
   summarize(
     Mean_Shell.mg = mean(Shell.mg),
@@ -127,13 +137,13 @@ Stats_MHW <- Olurida_CI3 %>%
 Stats_MHW
 
 ## plot
-ggplot(Olurida_CI3, aes(x=fMHW, y=CI, fill = fMHW)) + #factor MHW
+ggplot(Olurida_CI2, aes(x = MHW, y = CI, fill = MHW)) +
   geom_boxplot() +
   theme_classic() +
   scale_fill_brewer(palette = "RdYlBu", direction = -1)
 
 #### Grouped by SH_Tide ====
-Stats_SH_Tide <- Olurida_CI3 %>%
+Stats_SH_Tide <- Olurida_CI2 %>%
   group_by(SH_Tide) %>% 
   summarize(
     Mean_Shell.mg = mean(Shell.mg),
@@ -148,16 +158,15 @@ Stats_SH_Tide <- Olurida_CI3 %>%
 
 Stats_SH_Tide
 
-## plot with more reasonable outliers
-ggplot(Olurida_CI3, aes(x=fSH_Tide, y=CI, fill = fSH_Tide)) +
+## plot
+ggplot(Olurida_CI2, aes(x=SH_Tide, y=CI, fill = SH_Tide)) +
   geom_boxplot() +
   theme_classic() +
   scale_fill_brewer(palette = "Paired", direction = -1)
 
-
 #### Grouped by SH_Temp ====
 
-Stats_SHTemp <- Olurida_CI3 %>%
+Stats_SHTemp <- Olurida_CI2 %>%
   group_by(SH_Temp) %>% 
   summarize(
     Mean_Shell.mg = mean(Shell.mg),
@@ -170,16 +179,16 @@ Stats_SHTemp <- Olurida_CI3 %>%
     SD_CI = sd(CI),
     SE_CI = SD_CI/sqrt(n()))
 
-Stats_SH_Temp
+Stats_SHTemp
 
-## plot with more reasonable outliers
-ggplot(Olurida_CI3, aes(x=fSH_Temp, y=CI, fill = fSH_Temp)) +
+## plot
+ggplot(Olurida_CI2, aes(x=SH_Temp, y=CI, fill = SH_Temp)) +
   geom_boxplot() +
   theme_classic() +
   scale_fill_manual(values=c("#4575B4", "#FDAE61"))
 
 #### Grouped by SH_Tide, SH_Temp ====
-Stats_SH_TideTemp <- Olurida_CI3 %>%
+Stats_SH_TideTemp <- Olurida_CI2 %>%
   group_by(SH_Tide, SH_Temp) %>% 
   summarize(
     Mean_Shell.mg = mean(Shell.mg),
@@ -194,15 +203,15 @@ Stats_SH_TideTemp <- Olurida_CI3 %>%
 
 Stats_SH_TideTemp
 
-## plot with more reasonable outliers
-ggplot(Olurida_CI3, aes(x=fSH_Temp, y=CI, fill = fSH_Temp)) +
+## plot
+ggplot(Olurida_CI2, aes(x=SH_Temp, y=CI, fill = SH_Temp)) +
   geom_boxplot() +
   facet_wrap(~SH_Tide) +
   theme_classic() +
   scale_fill_manual(values=c("#4575B4", "#FDAE61"))
 
 #### Grouped by SH_Tide, SH_Temp, MHW ====
-Stats_SH_TideTemp_MHW <- Olurida_CI3 %>%
+Stats_SH_TideTemp_MHW <- Olurida_CI2 %>%
   group_by(SH_Tide, SH_Temp, MHW) %>% 
   summarize(
     Mean_Shell.mg = mean(Shell.mg),
@@ -215,13 +224,13 @@ Stats_SH_TideTemp_MHW <- Olurida_CI3 %>%
     SD_CI = sd(CI),
     SE_CI = SD_CI/sqrt(n()))
 
-Stats_SH_TideTemp_MHW 
+Stats_SH_TideTemp_MHW
 
 View(Stats_SH_TideTemp_MHW)
 
-ggplot(Olurida_CI3, aes(x=fMHW, y=CI, fill = fMHW)) +
+ggplot(Olurida_CI2, aes(x=MHW, y=CI, fill = MHW)) +
   geom_boxplot() +
-  facet_grid(fSH_Tide ~ fSH_Temp) +
+  facet_grid(SH_Tide ~ SH_Temp) +
   theme_classic() +
   theme(legend.position = "none") +
   scale_fill_brewer(palette = "RdYlBu", direction = -1) +
@@ -242,34 +251,36 @@ write_csv(Stats_SH_TideTemp_MHW , file = "data_output/O_lurida/Olurida_CI_MeanSD
 ## All Factors
 
 ## Fixed Factors: SH_Temp, SH_Tide, MHW
-m.SHtemp <- glm(CI ~ fSH_Temp, family = gaussian(link = "identity"), data = Olurida_CI3)
+m.SHtemp <- glm(CI ~ SH_Temp, family = gaussian(link = "identity"), data = Olurida_CI2)
 summary(m.SHtemp) #AIC: 4225.4
+tab_model(m.SHtemp)
 
-m.SHtide <- glm(CI ~ fSH_Tide, family = gaussian(link = "identity"), data = Olurida_CI3)
+m.SHtide <- glm(CI ~ SH_Tide, family = gaussian(link = "identity"), data = Olurida_CI2)
 summary(m.SHtide) #AIC: 4225.8
+tab_model(m.SHtide)
 
-m.MHW <- glm(CI ~ fMHW, family = gaussian(link = "identity"), data = Olurida_CI3)
+m.MHW <- glm(CI ~ MHW, family = gaussian(link = "identity"), data = Olurida_CI2)
 summary(m.MHW) #AIC: 4206.1
-
+tab_model(m.MHW)
 
 ## Fixed + Random (Tank) Factors
-m.all <- lmer(CI ~ fSH_Temp + fSH_Tide + fMHW + (1|Tank), data = Olurida_CI3)
-summary(m.all) #model output
-anova(m.all)
-AIC(m.all, m.SHtemp, m.SHtide, m.MHW) #m.all has LOWEST AIC: 4186.888
-tab_model(m.all)
+m.MHW.Tank <- lmer(CI ~ MHW + (1|Tank), data = Olurida_CI2)
+summary(m.MHW.Tank) #model output
+tab_model(m.MHW.Tank)
+AIC(m.MHW.Tank, m.SHtemp, m.SHtide, m.MHW) #m.MHW.TANK has LOWEST AIC: 4183.983
+
 
 ## Plot of Residuals
-plot(fitted(m.all), resid(m.all))
+plot(fitted(m.MHW.Tank), resid(m.MHW.Tank))
 abline(0,0)
 
 #plot(m.all)
 
 ## Q-Q plot of Residuals
-qqnorm(resid(m.all))
-qqline(resid(m.all)) 
+qqnorm(resid(m.MHW.Tank))
+qqline(resid(m.MHW.Tank)) 
 
 ## Density Plot of Residuals
-plot(density(resid(m.all)))
+plot(density(resid(m.MHW.Tank)))
 
 
